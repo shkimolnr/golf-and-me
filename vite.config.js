ee06b658af1c6e2b606400041480c4a632d50822
shkimolnr <shkim@olnr.kr>
2026-08-30 20:09:44 +0900

    Serve Vite assets from Sites client output

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { sites } from '@openai/sites-vite-plugin'
 
-export default defineConfig({ plugins: [react(), sites()] })
+export default defineConfig({
+  plugins: [react(), sites()],
+  build: { outDir: 'dist/client' },
+})
